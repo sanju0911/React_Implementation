2253f74b6188063fb21479ed65e97f4e352fcbc9
sanju <sanjayjanardhan231@gmail.com>
2024-07-01 13:39:47 +0530

    added card component

--- myapp/src/components/Users/AddUser.js
+++ myapp/src/components/Users/AddUser.js
@@ -1,25 +1,22 @@
-import React from 'react';
+import React from "react";
+import Card from "../UI/Card";
+import "./AddUser.css";
 
 const AddUser = () => {
   const addUserHandler = (event) => {
     event.preventDefault();
-    // Additional logic to handle form submission can be added here
   };
 
   return (
-    <div>
-      <form onSubmit={addUserHandler}>
-        <div>
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" />
-        </div>
-        <div>
-          <label htmlFor="age">Age</label>
-          <input type="number" id="age" />
-        </div>
+    <Card>
+      <form className="input" onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age</label>
+        <input id="age" type="number" />
         <button type="submit">Add User</button>
       </form>
-    </div>
+    </Card>
   );
 };
 
